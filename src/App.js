951ddc8b62d2bffd1512cfe494f72a09735b7dc2
David <david123rana@gmail.com>
2021-09-20 04:05:36 -0600

    context api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import { Route, Switch } from "react-router-dom"
-import About from './About';
-import Contact from './Contact';
-import Error from './Error';
-import Menu from "./Menu";
+import ComA from "./ComA";
+import React, { createContext } from "react";    {/* 1 - stap import kiya  */}
+
+const FirstName = createContext();   {/* 2 - create kiya */}
+const LastName = createContext();    {/* 5&6 - create kiya */}
+
 
 const App = () => {
-  const Name = () => {
-    return <h1>this is a name page</h1>
-  }
-
-
   return (
     <>
-    <Menu />
-    <Switch>     {/*9 - stap */}
-       <Route exact path="/"  component={About}/>  {/* 7 - stap set switch - route - path - component  10 - stap 10 exact path set krne ke liye /* */}
-       <Route exact path="/contact"  component={Contact}/>    {/* 8 - stap */}
-       <Route path="/contact/name"  component={Name}/>
-       <Route component={Error} />
-    </Switch>
-    {/* <About />
-    <Contact /> */}
+      <FirstName.Provider value={"David"}>   {/* stap 3 - provider set kiya */}
+        <LastName.Provider value={"Rana"}>
+        <ComA />              {/* stap 4 - component use kiya  */}
+        </LastName.Provider>
+      </FirstName.Provider>
     </>
-  )
-}
+  );
+};
 
 export default App;
+export { FirstName, LastName };
